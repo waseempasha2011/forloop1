c5af647a2782c39b762d70a2f03a456d60b18ea0
waseempasha2011 <142142208+waseempasha2011@users.noreply.github.com>
2023-08-13 15:01:39 +0530

    Initial commit

---
+++ forloop1.py
@@ -0,0 +1,12 @@
+#print the first 10 natural numbers using for loop
+#between 0 to 10
+# there are 11 numbers
+# therefore, we set the value
+# of n to 11
+n=11
+# since for loop starts with
+# the zero indexes we need to skip it and
+# start the loop from the first index
+
+for i in range(1,n):
+    print(i)
